e55f6c0703cc73cd4b4ea69df41cdcc517c0b3f5
Manuel Cau <caumanuel10@gmail.com>
2024-07-09 01:53:25 +0200

    swr

--- Fetch/Fetch/src/App.jsx
+++ Fetch/Fetch/src/App.jsx
@@ -3,11 +3,12 @@ import { GithubUsers } from "./GithubUsers";
 import { NewGithubUser } from "./NewGithubUser";
 import { Link, Routes, Route } from "react-router-dom";
 import { ShowGithubUser } from "./ShowGithubUser";
+import { SWRGithubUsers } from "./SWRGithubUsers";
 
 function App() {
   return (
     <>
-      <Link to="users/manuelcau">User</Link>
+      <Link to="/users">Users</Link>
       <br />
       <Link to="/NewGithubUser">New GithubUser</Link>
       <br />
@@ -17,7 +18,12 @@ function App() {
         <Route path="/" element={<GithubUsers />} />
         <Route path="/NewGithubUser" element={<NewGithubUser />} />
         <Route path="/CurrentLocation" element={<CurrentLocation />} />
-        <Route path="users/:username" element={<ShowGithubUser />} />
+        <Route path="/users" element={<SWRGithubUsers />}>
+          <Route index element={<h2>Add a user and select it</h2>} />
+          <Route path=":username" element={<ShowGithubUser />} />
+        </Route>
+
+        <Route path="*" element={<h1>404 not found</h1>} />
       </Routes>
     </>
   );
--- Fetch/Fetch/src/SWRGithubUsers.jsx
+++ Fetch/Fetch/src/SWRGithubUsers.jsx
@@ -1,20 +1,29 @@
 import useSWR from "swr";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function SWRGithubUsers() {
   const { data, error } = useSWR("https://api.github.com/users", fetcher);
+
+  const MyStyle = {
+    display: "flex",
+    flexDirection: "column",
+  };
 
   return (
     <div>
       {!data && !error && <h3>Loading...</h3>}
       {error && <h3>An error has occurred</h3>}
       {data && !error && (
-        <ul>
+        <div style={MyStyle}>
           {data.map((user) => (
-            <Link key={user.login}>{user.login}</Link>
+            <Link to={user.login} key={user.login}>
+              {user.login}
+            </Link>
           ))}
-        </ul>
+
+          <Outlet />
+        </div>
       )}
     </div>
   );
